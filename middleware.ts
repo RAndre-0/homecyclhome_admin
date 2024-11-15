53ec7f1d3fe2c25c08285a1853e3ed265366f8da
Raphaël ANDRÉ <=>
2024-11-15 13:49:19 +0100

    fix: middleware fix

--- middleware.ts
+++ middleware.ts
@@ -6,27 +6,24 @@ const PUBLIC_FILE = /\.(.*)$/; // Pour gérer les fichiers statiques (images, CS
 const PUBLIC_ROUTES = ['/login']; // Ajoutez ici les pages accessibles sans authentification
 
 export function middleware(request: NextRequest) {
-  // Récupérer le token des cookies
   const token = request.cookies.get('token')?.value;
 
-  // Vérifier si la route est publique
-  const isPublicRoute = PUBLIC_ROUTES.includes(request.nextUrl.pathname) || PUBLIC_FILE.test(request.nextUrl.pathname);
+  const isPublicRoute =
+    PUBLIC_ROUTES.includes(request.nextUrl.pathname) || PUBLIC_FILE.test(request.nextUrl.pathname);
 
-  // Si la route est publique, pas besoin de vérifier l'authentification
   if (isPublicRoute) {
     return NextResponse.next();
   }
 
-  // Si le token est absent ou invalide, rediriger vers la page de login
   if (!token) {
     const loginUrl = new URL('/login', request.url);
+    console.log('Redirecting to:', loginUrl.toString());
     return NextResponse.redirect(loginUrl);
   }
-  
-  // Si le token est présent, continuer avec la requête
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/'], // Applique le middleware à toutes les pages sauf celles définies ici
+  matcher: ['/((?!api|login|_next/static|_next/image|favicon.ico).*)'],
 };
